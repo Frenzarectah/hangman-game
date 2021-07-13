2b27aac50196585719b6a356c35ed4bc0d45aa92
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-07-13 17:27:35 +0200

    adding - a startGame funct -a new html section -some style

--- js/script.js
+++ js/script.js
@@ -1,4 +1,16 @@
-const words = [{word:"sedia",hint:"si usa per poggiare le chiappe"},
-                {word:"mucca",hint:"si mangia"}];
-             
-console.log(words[1].hint);
+const guess = [{word:"sedia",hint:"si usa per poggiare le chiappe"},
+                {word:"mucca",hint:"si mangia"},
+                {word:"ciola",hint:"si succhia"},
+                {word:"bastianic",hint:"so duri sti gnocchetti"},
+                {word:"cracco",hint:"tu sei un culo"},
+            ];
+var startGame = () =>{
+    var button = document.querySelector(".start_btn");
+    button.style.display="none";
+    var randomizedIdx = Math.round(Math.random() * guess.length-1);
+    var writing = "";
+    for (i=0;i<=((guess[randomizedIdx].word).length)-1;i++)  writing+="_ ";
+    var hint_div = document.querySelector(".hint");
+    hint_div.append(writing);
+    
+};
