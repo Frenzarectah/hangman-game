62ce6f4810a76eedd087aa8b8e9a92cf25741923
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-07-14 17:25:51 +0200

    adding -html new div -a new appending on startGame funct -a viewport setting

--- js/script.js
+++ js/script.js
@@ -7,10 +7,12 @@ const guess = [{word:"sedia",hint:"si usa per poggiare le chiappe"},
 var startGame = () =>{
     var button = document.querySelector(".start_btn");
     button.style.display="none";
-    var randomizedIdx = Math.round(Math.random() * guess.length-1);
+    var rndomIdx = Math.round(Math.random() * guess.length-1);
     var writing = "";
-    for (i=0;i<=((guess[randomizedIdx].word).length)-1;i++)  writing+="_ ";
+    for(i=0;i<=((guess[rndomIdx].word).length)-1;i++)  writing+="_ ";
+    var word_div = document.querySelector(".word_div");
     var hint_div = document.querySelector(".hint");
     hint_div.append(writing);
-    
+    word_div.append(guess[rndomIdx].hint);
+    rndomIdx = Math.round(Math.random() * guess.length-1);
 };
