35d7f8627af139ebbd9f2400099fff4fc517067a
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-07-16 17:13:28 +0200

    modified -merged 2 divs into one -modified code with backtick write method

--- js/script.js
+++ js/script.js
@@ -3,16 +3,25 @@ const guess = [{word:"sedia",hint:"si usa per poggiare le chiappe"},
                 {word:"ciola",hint:"si succhia"},
                 {word:"bastianic",hint:"so duri sti gnocchetti"},
                 {word:"cracco",hint:"tu sei un culo"},
+                {word:"cannavacciuolo",hint:"e disc sin!"},
             ];
 var startGame = () =>{
     var button = document.querySelector(".start_btn");
     button.style.display="none";
+    var br = document.createElement("br");
     var rndomIdx = Math.round(Math.random() * guess.length-1);
     var writing = "";
     for(i=0;i<=((guess[rndomIdx].word).length)-1;i++)  writing+="_ ";
-    var word_div = document.querySelector(".word_div");
     var hint_div = document.querySelector(".hint");
-    hint_div.append(writing);
-    word_div.append(guess[rndomIdx].hint);
+    hint_div.innerHTML=`<h2>PAROLA: ${writing}</h2>
+                        <h2>SUGGERIMENTO: ${guess[rndomIdx].hint}</h2>
+    `;
     rndomIdx = Math.round(Math.random() * guess.length-1);
+};
+document.onload = ()=>{
+keyb = document.querySelectorAll(".btn");
+console.log(keyb);
+keyb.addEventListener("click",()=>{
+    console.log("hai cliccato qualcosa qui su keyboard!");
+});
 };
