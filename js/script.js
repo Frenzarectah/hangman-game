4dcdeb19d1385d3fcacd2a8e5b0fbe1be535f605
Frenzarectah <sorrentino.francesco87@gmail.com>
2021-07-22 17:35:02 +0200

    new change

--- js/script.js
+++ js/script.js
@@ -8,32 +8,34 @@ const guess = [{word:"sedia",hint:"si usa per poggiare le chiappe"},
             ];
 
 var startGame = () =>{
+    var parola = [];
     var button = document.querySelector(".start_btn");
     button.style.display="none";
     var rndomIdx = Math.round(Math.random() * guess.length-1);
-    var writing = "";
-    for(i=0;i<=((guess[rndomIdx].word).length)-1;i++)  writing+="_ ";
+    for(i=0;i<=((guess[rndomIdx].word).length)-1;i++){
+        parola[i]="_";
+    }  
     var hint_div = document.querySelector(".hint");
     worde = guess[rndomIdx].word;
-    hint_div.innerHTML=`<h2>PAROLA:</h2> <h2>${writing}</h2>
+    hint_div.innerHTML=`<h2>PAROLA:</h2> <h2>${parola.join(" ")}</h2>
                         <h2>SUGGERIMENTO: ${guess[rndomIdx].hint}</h2>
     `;
     rndomIdx = Math.round(Math.random() * guess.length-1);
+    console.log(parola);
 };
 var search = (word,letter) =>{
-    var parola;
-    var parola1 = [];
-    if (word.includes(letter) === true){
-        var pos = word.indexOf(letter);
-        var parola = document.getElementsByTagName("h2")[1].innerHTML;
-        console.log("la word è al momento"+parola);
-        for(i=0;i<=word.length-1;i++){
-            if(i===pos){ parola1[i]=letter;
-            }else parola1[i]="_";
-        }
-        console.log(parola1+" dopo replace che non funge un cazzo");
-        console.log("la lettera "+letter+" è presente in "+word+" in posizione "+pos);
-    } 
+    var parola = [];
+        if (word.includes(letter) === true){
+            var pos = word.indexOf(letter);
+            for(i=0;i<=word.length-1;i++){
+                var ausil = document.getElementsByTagName("h2")[1].innerHTML;
+                parola[i] = ausil[i];    
+                console.log(ausil);
+            }
+            //var parola = document.getElementsByTagName("h2")[1].innerHTML; // recupero dell'array parola in startGame();
+            //console.log(parola[1]);
+            console.log("la lettera "+letter+" è presente in "+word+" in posizione "+pos);
+        }else console.log("CIAO"); 
 }
 var extractNum = () =>{
     var letter = "";
@@ -41,7 +43,7 @@ key = document.querySelectorAll(".btn");
 key.forEach((element) =>{
     element.addEventListener("click",()=>letter = addEvent(element.value));
 });
-};
+}
 var addEvent=(key)=>{
     console.log("pulsante premuto:"+key);
     search(worde,key);
