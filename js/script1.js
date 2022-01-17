e2674a49cd773d93178ce60f408f8a175a79a456
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-01-17 17:57:17 +0100

    adjusting some code

--- js/script1.js
+++ js/script1.js
@@ -1,30 +1,30 @@
 
-const guess = [{word:"sedia",hint:"si usa per poggiare le chiappe"},
-{word:"scontrino",hint:"permette di riscattare la garanzia"},
-{word:"monitor",hint:"indispensabile per usare il computer"},
-{word:"treppiedi",hint:"si usa per diminuire gli scossoni durante le foto"},
-{word:"cracco",hint:"famoso cuoco stellato"},
-{word:"idraulico",hint:"è famoso quello dei videogiochi"},
-{word:"google",hint:"il sito web pii' usato del mondo"},
-{word:"tappetino",hint:"si usa sia per il mouse che per i pavimenti"},
-{word:"michelangelo",hint:"famoso scultore italiano"},
-{word:"reykjavik",hint:"capitale europea nordica"},
+const guess = [ {word:"sedia",hint:"si usa per poggiare le chiappe"},
+                {word:"scontrino",hint:"permette di riscattare la garanzia"},
+                {word:"monitor",hint:"indispensabile per usare il computer"},
+                {word:"treppiedi",hint:"si usa per diminuire gli scossoni durante le foto"},
+                {word:"cracco",hint:"famoso cuoco stellato"},
+                {word:"idraulico",hint:"è famoso quello dei videogiochi"},
+                {word:"google",hint:"il sito web pii' usato del mondo"},
+                {word:"tappetino",hint:"si usa sia per il mouse che per i pavimenti"},
+                {word:"michelangelo",hint:"famoso scultore italiano"},
+                {word:"reykjavik",hint:"capitale europea nordica"},
 ];
 var attempts = 0;
 
 var startGame = () =>{
-var guessWrd = [];
-var button = document.querySelector(".start_btn");
-button.style.display="none";
-var rndomIdx = Math.round(Math.random() * guess.length-1);
-for(i=0;i<=((guess[rndomIdx].word).length)-1;i++){
-guessWrd[i]="_";
+    var guessWrd = [];
+    var button = document.querySelector(".start_btn");
+    button.style.display="none";
+    var rndomIdx = Math.round(Math.random() * guess.length-1);
+    for(i=0;i<=((guess[rndomIdx].word).length)-1;i++){
+        guessWrd[i]="_";
 }  
-var hint_div = document.querySelector(".hint");
-worde = guess[rndomIdx].word; //parola in chiaro da indovinare
-hint_div.innerHTML=`<h2>PAROLA:</h2> <h2>${guessWrd.join("")}</h2>
-                    <h2>SUGGERIMENTO: ${guess[rndomIdx].hint}</h2>`;
-rndomIdx = Math.round(Math.random() * guess.length-1);
+    var hint_div = document.querySelector(".hint"); 
+    worde = guess[rndomIdx].word; //parola in chiaro da indovinare
+    hint_div.innerHTML=`<h2>PAROLA:</h2> <h2>${guessWrd.join("")}</h2> //eliminazione delle virgole tra i vari elementi dell'array
+                        <h2>SUGGERIMENTO: ${guess[rndomIdx].hint}</h2>`;
+    rndomIdx = Math.round(Math.random() * guess.length-1);
 };
 
 var search =(word,letter)=>{
@@ -63,18 +63,18 @@ var notGuessing = () =>{
     } 
 }
 var addingEvent = () =>{
-var letter = "";
-key = document.querySelectorAll(".btn");
-key.forEach((element) =>{
-element.addEventListener("click",()=>letter = addedEvent(element));
-});
+    var letter = "";
+    key = document.querySelectorAll(".btn");
+    key.forEach((element) =>{
+        element.addEventListener("click",()=>letter = addedEvent(element));
+    });
 }
 
 var addedEvent=(key)=>{
-console.log("pulsante premuto:"+key.value);
-key.disabled="true";
-search(worde.toUpperCase(),key.value);
-return true;
+    console.log("pulsante premuto:"+key.value);
+    key.disabled="true";
+    search(worde.toUpperCase(),key.value);
+    return true;
 }
 
 module.export={search,startGame};
