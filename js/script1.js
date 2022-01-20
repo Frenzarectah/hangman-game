fc6502720f37826a96973e0cd0f8712539219f2f
Frenzarectah <sorrentino.francesco87@gmail.com>
2022-01-20 10:51:30 +0100

    changing some graphic elements

--- js/script1.js
+++ js/script1.js
@@ -10,17 +10,17 @@ const guess = [ {word:"sedia",hint:"si usa per poggiare le natiche"},
                 {word:"michelangelo",hint:"famoso scultore italiano"},
                 {word:"reykjavik",hint:"capitale europea nordica"},
 ];
-var attempts = 0;
-
-var startGame = () =>{
-    var guessWrd = [];
-    var button = document.querySelector(".start_btn");
+let attempts = 0;
+let worde;
+const startGame = () =>{
+    let guessWrd = [];
+    let button = document.querySelector(".start_btn");
     button.style.display="none";
-    var rndomIdx = Math.round(Math.random() * guess.length-1);
+    let rndomIdx = Math.round(Math.random() * guess.length-1);
     for(i=0;i<=((guess[rndomIdx].word).length)-1;i++){
         guessWrd[i]="_";
 }  
-    var hint_div = document.querySelector(".hint"); 
+    let hint_div = document.querySelector(".hint"); 
     worde = guess[rndomIdx].word; //parola in chiaro da indovinare
     hint_div.innerHTML=`<h2>PAROLE:</h2> <h2>${guessWrd.join("")}</h2> 
                         <h2>SUGGERIMENTO: ${guess[rndomIdx].hint}</h2>`;
@@ -28,51 +28,51 @@ var startGame = () =>{
     return 5; //prova per unit test
 };
 
-var search =(word,letter)=>{
-    var guessed = false;
-    var DomWord = document.getElementsByTagName("h2")[1].innerHTML;
-    var parola = []; 
+const search =(word,letter)=>{
+    let guessed = false;
+    let DomWord = document.getElementsByTagName("h2")[1].innerHTML;
+    let parola = []; 
     for(i=0;i<=DomWord.length-1;i++) parola[i] = DomWord[i]; //creazione array da stringa per manipolazione
     for(i=0;i<=word.length-1;i++){
         if (word[i] === letter){
             parola[i] = letter;
-            var guessed = true;
+               guessed = true;
         }
     }
     guessing(guessed,parola);
     return guessed;
 }
 
-var guessing =(guessBool,guessWord) =>{
+let guessing =(guessBool,guessWord) =>{
     if(guessBool === true){
         guessWord = guessWord.join(""); //elimina virgole da array
         document.getElementsByTagName("h2")[1].innerHTML = guessWord;
         if (guessWord.includes("_") === false){ //se la stringa da indovinare non contiene piu' underscore si considera vinta la partita
-            document.getElementsByClassName("amatic_title")[0].innerHTML = "EVVIVA HAI SALVATO IL MORTO!";
+            document.getElementsByClassName("title")[0].innerHTML = "EVVIVA HAI SALVATO IL MORTO!";
             document.getElementById("retry").style.display="block"; 
         }
     }else notGuessing();
 }
-var notGuessing = () =>{
+const notGuessing = () =>{
     if(attempts<4){
     attempts++;
-    var page = document.querySelector("#page");
+    let page = document.querySelector("#page");
     page.style.backgroundImage='url(imgs/imgs_once/image'+attempts+'.webp)';  //cambia sfondo
-    }else{  document.getElementsByClassName("amatic_title")[0].innerHTML=" GAME OVER!";
+    }else{  document.getElementsByClassName("title")[0].innerHTML=" GAME OVER!";
             document.getElementsByClassName("main_container")[0].style.display="none";    
             document.getElementsByClassName("keyboard")[0].style.display="none";
             document.getElementById("retry").style.display="block"; 
     } 
 }
-var addingEvent = () =>{    //funzione che aggiunge a tutti i tasti della tastiera 
-    var letter = "";        //l'evento addEvent di cui sotto
+const addingEvent = () =>{    //funzione che aggiunge a tutti i tasti della tastiera 
+    let letter = "";        //l'evento addEvent di cui sotto
     key = document.querySelectorAll(".btn");
     key.forEach((element) =>{
         element.addEventListener("click",()=>letter = addedEvent(element));
     });
 }
 
-var addedEvent=(key)=>{  
+const addedEvent=(key)=>{  
     console.log("pulsante premuto:"+key.value);
     key.disabled="true";
     search(worde.toUpperCase(),key.value); //search prende come parametro la parola da indovinare e la lettera 
